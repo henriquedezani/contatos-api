23a5d19da9487d6a4a3ec5e3338fd07ace9be1b6
Henrique Dezani <dezani@gmail.com>
2021-04-13 20:36:12 +0000

    Vesão com banco de dados postgres

---
+++ database.js
@@ -0,0 +1,47 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: 'hdjhahyopcdjpq',
+    password: 'f24352b0d80c4bca55f690d3b4922c37e53efec3aba3d52ec524b8f6ac65a310',
+    host: 'ec2-52-1-115-6.compute-1.amazonaws.com',
+    database: 'das98vipo52onh',
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+});
+
+// const script = `
+//     CREATE TABLE IF NOT EXISTS contatos
+//     (
+//         ID serial primary key,
+//         nome varchar(60) not null,
+//         telefone varchar(20) not null
+//     )
+// `;
+
+
+// pool.query(script, function(error, result) {
+//     if(error)
+//         throw error;
+    
+//     console.log('Tabela criada com sucesso.');
+// })
+
+module.exports = {
+
+    async create(nome, telefone) {
+        try {
+            const sql = `INSERT INTO contatos (nome, telefone) VALUES ($1, $2)`;
+            const result = await pool.query(sql, [nome, telefone]);
+            return result.rows;
+        }catch(error) {
+            console.log(error);
+            return -1;
+        }
+    },
+
+    async read() {
+        const sql = `SELECT * FROM contatos order by nome`;
+        const result = await pool.query(sql);
+        return result.rows;
+    }
+}
--- server.js
+++ server.js
@@ -1,38 +1,30 @@
 const express = require('express'); // import express; using express;
-const { uuid } = require('uuidv4');
-
 const server = express();
+
+const database = require('./database');
 
 // middleware
 server.use(express.json())
 
-contatos = [];
-
 // localhost:3000/
-server.get('/', function(request, response) {
+server.get('/', async function(request, response) {
+    const contatos = await database.read();
     response.json(contatos);
 })
 
 // localhost:3000/123o9123-123-1-1231233
-server.get('/:id', function(request, response) {
-    const id = request.params.id;
-    const result = contatos.filter(contato => contato.id ==  id);
-    response.json(result);
-})
+// server.get('/:id', function(request, response) {
+//     const id = request.params.id;
+//     const result = contatos.filter(contato => contato.id ==  id);
+//     response.json(result);
+// })
 
 // localhost:3000/
-server.post('/', function(request, response) {
+server.post('/', async function(request, response) {
     const nome = request.body.nome;
     const telefone = request.body.telefone;
 
-    var contato = {
-        id: uuid(),
-        nome,
-        telefone
-    };
-
-    contatos.push(contato);
-
+    const result = await database.create(nome, telefone);
     response.status(201).send();
 })
 
