7f9e469030b7a7e7470d06615e55954efed055bb
Henrique Dezani <dezani@gmail.com>
2021-04-13 19:05:40 +0000

    Versão da aula 04-13

--- server.js
+++ server.js
@@ -8,20 +8,24 @@ server.use(express.json())
 
 contatos = [];
 
+// localhost:3000/
 server.get('/', function(request, response) {
     response.json(contatos);
 })
 
+// localhost:3000/123o9123-123-1-1231233
 server.get('/:id', function(request, response) {
     const id = request.params.id;
-    response.json(id);
+    const result = contatos.filter(contato => contato.id ==  id);
+    response.json(result);
 })
 
+// localhost:3000/
 server.post('/', function(request, response) {
     const nome = request.body.nome;
     const telefone = request.body.telefone;
 
-    contato = {
+    var contato = {
         id: uuid(),
         nome,
         telefone
